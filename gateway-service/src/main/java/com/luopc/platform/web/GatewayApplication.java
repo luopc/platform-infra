2d57dbe3537f8e880f1ca7b8f6373355e11baff4
Robin Luo <robin_luopc@163.com>
2025-12-09 20:51:55 +0800

    [2025.12.09] add eureka server

--- gateway-service/src/main/java/com/luopc/platform/web/GatewayApplication.java
+++ gateway-service/src/main/java/com/luopc/platform/web/GatewayApplication.java
@@ -1,9 +1,11 @@
 package com.luopc.platform.web;
 
+import com.luopc.platform.web.config.BootStaticWebMvcConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {BootStaticWebMvcConfiguration.class})
 public class GatewayApplication {
 
     public static void main(String[] args) {
